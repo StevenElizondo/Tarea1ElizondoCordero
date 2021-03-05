d6e3f22a9e5654579b763c25efbf56fde5fa0725
Steven Elizondo <stevenelizondo855@gmail.com>
2021-03-04 20:46:56 -0600

    Cuarto commit v2
    corrección verdadera de los errores, no se corrigieron en el anterior porque no había guardado el archivo

--- error.py
+++ error.py
@@ -1,9 +1,11 @@
 def funcionErr():
+    f = 5
     # error detectable por flake8 corregido: falta espacio entre operador
     i = 0
-    while(true):
+    while(True):
         i == i+1
     # error detectable por flake8 corregido: sintaxis erronea es doble igual
     if(i == 4):
-        # error detectable por flake8 corregido: falta espacio despues de la coma
+        # error detectable por flake8 corregido:
+        # falta espacio despues de la coma
         print("hola mundo ", f)   
